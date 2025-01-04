41b82ef657a5ea54a09e341b3ccd889f32e7c6cf
Yury S <yurafikys@gmail.com>
2025-01-04 14:29:00 +0300

    feat: Getting coffee list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Button, Card, Rate, Tag } from "antd";
+import { useEffect } from "react";
 import "./App.css";
+import { useCoffeeStore } from "./model/coffeeStore";
 
 function App() {
-  return <></>;
+  const { getCoffeeList, coffeeList } = useCoffeeStore();
+
+  useEffect(() => {
+    getCoffeeList();
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <div className="cardsContainer">
+        {coffeeList &&
+          coffeeList.map((coffee) => (
+            <Card
+              key={coffee.id}
+              cover={<img src={coffee.image} alt={coffee.name} />}
+              actions={[
+                <Button icon={<ShoppingCartOutlined />}>{coffee.price}</Button>,
+              ]}
+            >
+              <Card.Meta title={coffee.name} description={coffee.subTitle} />
+              <Tag color="purple" style={{ marginTop: 12 }}>
+                {coffee.type}
+              </Tag>
+              <Rate
+                defaultValue={coffee.rating}
+                disabled
+                allowHalf
+                style={{ marginTop: 12 }}
+              />
+            </Card>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/model/coffeeStore.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { create, StateCreator } from "zustand";
+import { devtools } from "zustand/middleware";
+import { CoffeeType } from "../types/coffeeTypes";
+
+const BASE_URL = "https://purpleschool.ru/coffee-api";
+
+type CoffeeState = {
+  coffeeList?: CoffeeType[];
+};
+
+type CoffeeAction = {
+  getCoffeeList: () => void;
+};
+
+const coffeeSlice: StateCreator<
+  CoffeeState & CoffeeAction,
+  [["zustand/devtools", never]]
+> = (set) => ({
+  coffeeList: undefined,
+  getCoffeeList: async () => {
+    try {
+      const { data } = await axios.get(BASE_URL);
+      set({ coffeeList: data });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+});
+
+export const useCoffeeStore = create<CoffeeState & CoffeeAction>()(
+  devtools(coffeeSlice),
+);
