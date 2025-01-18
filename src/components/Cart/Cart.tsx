1251e428f7786a719af2587e169ab6d50921e11e
Yury S <yurafikys@gmail.com>
2025-01-18 22:44:43 +0300

    fix: Fix url

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -36,7 +36,10 @@ export const Cart = () => {
 								className={styles.deleteIcon}
 								onClick={() => deleteToCart(item.id)}
 							>
-								<img src="/public/icon/delete-icon.svg" alt="delete-icon" />
+								<img
+									src="https://raw.githubusercontent.com/fikyslsgg/app-coffee/9def8d43e6c92a334175025168caa9f726a89417/public/icon/delete-icon.svg"
+									alt="delete-icon"
+								/>
 							</Button>
 						</div>
 					))}
